fcb4005aa01ed88a3dba307ba05052658f7c457e
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-25 15:39:09 -0400

    created validations

--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+validates :name, presence: true
+validates :discription, presence: true
+validates :price_in_cents, presence: true
+validates :price_in_cents, numericality: {only_integer: true}
+
 end
