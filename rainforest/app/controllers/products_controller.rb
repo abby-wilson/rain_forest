f6ed428b44fafa1853762aec09a383adddb48ccb
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-25 14:56:12 -0400

    Added root route

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -1,24 +1,25 @@
 class ProductsController < ApplicationController
 
 def index
+  # @products = Products.all
 end
 
-def create
-end
-
-def new
-end
-
-def edit
-end
-
-def show
-end
-
-def update
-end
-
-def destroy
-end
+# def create
+# end
+#
+# def new
+# end
+#
+# def edit
+# end
+#
+# def show
+# end
+#
+# def update
+# end
+#
+# def destroy
+# end
 
 end
--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  #
   resources :products
+
+  # get '/' => 'products#index'
+
+  root 'products#index'
+
+  # get '/products' => 'products#index', as: 'products'
 
 end
